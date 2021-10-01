17ff4e03f5974052821cf5d6920724da9172d9d4
Prio Pambudi <priopambudi869@gmail.com>
2021-10-01 10:30:28 +0700

    little change

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { NavLink } from 'react-router-dom';
 
 export const Category = ({ setStatus }) => {
   const [isActiveAll, setIsActiveAll] = useState(false);
